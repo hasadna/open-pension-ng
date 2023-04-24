8f2f5600bc526c55d46ee0cd2dfc6cdd94a26e84
Nitzan Raz <nitz.raz@gmail.com>
2023-04-24 14:50:40 +0300

    created rudimentary service

--- djang/importer/models.py
+++ djang/importer/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Company(models.Model):
-    pass
+    name = models.CharField(maxlength=20)
 
 class Report(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
---
+++ djang/importer/services/downloader.py
@@ -0,0 +1,12 @@
+from .. import models
+
+def download(file_id):
+    """
+    # Obviously change this
+    company, _ = models.Company.objects.get_or_create(name="aaaaaa")
+    models.Report(
+        company=company,
+        submission_date=timezone.now(),
+    ).save()
+    """
+    pass
