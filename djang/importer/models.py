7c667a7d09c16f78cfdaa18be4a65691dec9d72d
Nitzan Raz <nitz.raz@gmail.com>
2023-05-08 22:14:55 +0300

    fixed typo

--- djang/importer/models.py
+++ djang/importer/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Company(models.Model):
-    name = models.CharField(maxlength=20)
+    name = models.CharField(max_length=20)
 
 class Report(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
